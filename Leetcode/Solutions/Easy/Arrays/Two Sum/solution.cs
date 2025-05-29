be88219aa372e81cea47a51e21d0e42f8fd019cf
PTKhuong96 <phankhuong96@gmail.com>
2025-05-29 23:12:49 +0700

    update solution using hash map.

--- Leetcode/Solutions/Easy/Arrays/Two Sum/solution.cs
+++ Leetcode/Solutions/Easy/Arrays/Two Sum/solution.cs
@@ -13,3 +13,29 @@ public class Solution {
         return new int[0];
     }
 }
+
+
+--USING HASH MAP--
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numbersDic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int findingNumber = target - nums[i];
+            if (numbersDic.ContainsKey(findingNumber))
+            {
+                return new int[]{i, numbersDic[findingNumber]};
+            }
+
+            numbersDic.Add(nums[i], i);
+        }
+
+        return null;
+    }
+}
+// for each number
+// int current number = nums[i].
+// number need to find = target - nums[i]
+// if dic has that => return {i, dic[number need to find]}
+// if dic could not find that => dic.add(current number, i);
